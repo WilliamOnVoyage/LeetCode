93a9df834dfe6d3e4e14f6a675682307e36a5530
Zhou Moliang <endeavourwilliam.zhou@gmail.com>
2017-02-08 21:11:43 -0500

    2017.02.08

--- src/Easy/Solution.java
+++ src/Easy/Solution.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Stack;
 
 public class Solution {
 
@@ -1008,4 +1009,51 @@ public class Solution {
 		r.add(1);
 		return r;
 	}
+
+	public class MyQueue {
+		Stack<Integer> s1;
+		Stack<Integer> s2;
+
+		/** Initialize your data structure here. */
+		public MyQueue() {
+			s1 = new Stack<>();
+			s2 = new Stack<>();
+
+		}
+
+		/** Push element x to the back of queue. */
+		public void push(int x) {
+			while (!s2.isEmpty()) {
+				s1.push(s2.pop());
+			}
+			s1.push(x);
+		}
+
+		/**
+		 * Removes the element from in front of queue and returns that element.
+		 */
+		public int pop() {
+			while (!s1.isEmpty()) {
+				s2.push(s1.pop());
+			}
+			return s2.pop();
+		}
+
+		/** Get the front element. */
+		public int peek() {
+			if (!s1.isEmpty()) {
+				while (!s1.isEmpty()) {
+					s2.push(s1.pop());
+				}
+			}
+			return s2.peek();
+
+		}
+
+		/** Returns whether the queue is empty. */
+		public boolean empty() {
+			return s1.isEmpty() && s2.isEmpty();
+		}
+	}
+
 }
